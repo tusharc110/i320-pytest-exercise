27fe3c5348cee8fb60e8899f1901c52b880d1660
tusharc110 <tusharc110@users.noreply.github.com>
2026-03-13 16:56:25 -0400

    Add additional formatting assertions and failing invalid-format tests; next update function validation

--- my_code.py
+++ my_code.py
@@ -1,14 +1,26 @@
 import pytest
 
+
 def fix_phone_num(phone_num_to_fix):
   # given "5125558823". Split the parts, then recombine and return
-  area_code = phone_num_to_fix[0:3] # 512 (first three digits)
-  three_part = phone_num_to_fix[3:6] # 555 (next three digits)
-  four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
-  
-  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
-  
+  area_code = phone_num_to_fix[0:3]  # 512 (first three digits)
+  three_part = phone_num_to_fix[3:6]  # 555 (next three digits)
+  four_part = phone_num_to_fix[6:]  # 8823 (last four digits)
+
+  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part
+
   return fixed_num
+
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  assert fix_phone_num("5554429876") == '(555) 442 9876'
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
+
+
+def test_raises_error_on_badly_formatted_phone_inputs():
+  with pytest.raises(ValueError):
+    fix_phone_num("555-442-98761")
+
+  with pytest.raises(ValueError):
+    fix_phone_num("(3213) 654 3333")
